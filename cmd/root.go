806285b3b14a076f849a49fba73368aba2dbe590
ahmetzumber <ahmetzumber5@gmail.com>
2022-05-23 13:21:52 +0300

    first command added

--- cmd/root.go
+++ cmd/root.go
@@ -5,26 +5,20 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "rapid-note-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Rapid note provides you taking a notes dynamically from terminal.",
+	Long:  `A longer description that rapid note loves you.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello from Rapid World !!")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +41,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
