a6814ec833c08317b6a629b8a6c8d2607a799cb6
ahmetzumber <ahmetzumber5@gmail.com>
2022-06-13 22:27:47 +0300

    postgre db creation test

--- internal/postgre/postgre_test.go
+++ internal/postgre/postgre_test.go
@@ -1,1 +1,12 @@
 package postgre
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateNewPostgreDBConnectionSuccesfully(t *testing.T){
+	db, err := NewPostgreDB(Config)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+}
