8aeaf02bbe8abf8b93fbd20c2b722bc30ae9abd1
ahmetzumber <ahmetzumber5@gmail.com>
2022-06-13 23:32:17 +0300

    trash fields removed

--- internal/user/user.go
+++ internal/user/user.go
@@ -4,12 +4,3 @@ type User struct {
 	Username 	string
 	Email	 	string
 }
-
-var Users []string
-
-func Create(username string) string {
-	newUser := User{
-		Username: username,
-	}
-	return newUser.Username
-}
